357555c8e53c3a30b8dcc8a9ebec4e1901f8c072
maybe <maybe>
2021-05-01 16:23:18 -0400

    Includes the source code.

---
+++ CIS1202-Week15/Source.cpp
@@ -0,0 +1,31 @@
+// Andrew Cole
+// CIS 1202
+// May 1st, 2021
+
+#include <iostream>
+using namespace std;
+
+char character(char start, int offset)
+{
+	return 0;
+}
+
+// Main function
+
+int main()
+{
+	char start;
+	int offset;
+
+	cout << "Enter a letter: ";
+	cin >> start;
+	cout << "Enter a number:";
+	cin >> offset;
+
+	cout << endl;
+
+	cout << start << " and " << offset << endl;
+
+	system("pause");
+	return 0;
+}
